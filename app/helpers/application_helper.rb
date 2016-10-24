4857a5593bb1f17dadb333c5ac08bd728eb222ef
jsmorales <jhellmetal2000@gmail.com>
2016-10-24 14:51:09 -0500

    Muestra los datos del usuario con un gravatar y los articulos relacionados a el.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def gravatar_for(user_objt, options = { size: 80 })
+		gravatar_id = Digest::MD5::hexdigest(user_objt.email.downcase)
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user_objt.username, class: "img-thumbnail")
+	end
+
 end
