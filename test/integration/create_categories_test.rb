2a1821fe234abb5b0b635b8829444f2296a0f37f
jsmorales <jhellmetal2000@gmail.com>
2016-11-08 23:23:49 -0500

    clase 134, test añadir categoria invalida, verifica que salga mensajes de error con esta excepcion.

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+	#test en caso de que se pueda crear perfectamente una
+	#categoría
+
 	test 'get formulario y crear categoria' do
 		#va al formulario
 		get new_category_path
@@ -18,6 +21,32 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 		#en esta pagina debe aparecer el nombre de la 
 		#categoria que se envio
 		assert_match "tecnologia", response.body
+
+	end
+
+	#test en caso de excepción, error
+
+	test 'categoria invalida resultados en error' do
+
+		#va al formulario
+		get new_category_path
+		#revisa que este la pagina
+		assert_template 'categories/new'
+
+		#se envia una peticion post a categories_path
+		#y que no halla diferencia en el count
+		assert_no_difference 'Category.count' do
+			#le envia un nombre vacío
+			post categories_path, category: {name: " "}
+		end
+
+		#redirecciona a new segun el controlador
+		assert_template 'categories/new'
+		#selecciona los mensajes de error producidos por
+		#la excepcion, selecciona el DOM, estos elementos son
+		#los que se crean en el parcial shared/_errors.html.erb 
+		assert_select 'h3.panel-title'
+		assert_select 'div.panel-body'
 
 	end
 
